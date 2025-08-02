7449331a5e73d95e93d4306fdb9340133a2d8fa9
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-08-03 00:38:54 +0530

    Main class implemented

--- src/main/java/com/record/PatientRecordSystemApplication.java
+++ src/main/java/com/record/PatientRecordSystemApplication.java
@@ -2,7 +2,7 @@ package com.record;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Main class
 @SpringBootApplication
 public class PatientRecordSystemApplication {
 
