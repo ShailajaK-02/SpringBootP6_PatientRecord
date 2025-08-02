c99ccbb600e859ce7b48f449ff6cdb759e649f73
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-08-03 00:36:47 +0530

    service impl commit

--- src/main/java/com/record/serviceImpl/PatientServiceImpl.java
+++ src/main/java/com/record/serviceImpl/PatientServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+//service implementation class
 
 @Service
 public class PatientServiceImpl implements PatientService
